8e707430a97b7dbcb45a245274674669786b57bb
Z4 DEV <z4kreyyt@gmail.com>
2023-10-12 00:45:57 +0100

    Remove unecessary space

--- src/main/java/pt/Z4/api/User/UserController.java
+++ src/main/java/pt/Z4/api/User/UserController.java
@@ -1,6 +1,5 @@
 package pt.Z4.api.User;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
